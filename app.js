646cf43508f94d28f22a4054982038c01289ec33
Daryll Ricalde <rstevendaryll@gmail.com>
2021-05-29 00:35:16 +1000

    find bugs in app.js

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var pics = [
 	"Ollie/2.jpg",
 	"Ollie/3.jpg",
 	"Ollie/4.jpg",
-	"Ollie/5.JPG",
-	"Ollie/6.JPG",
-	"Ollie/7.JPG",
-	"Ollie/8.JPG",
-	"Ollie/9.JPG",
-	"Ollie/10.JPG"
-] // An array of photos
+	"Ollie/5.jpg",
+	"Ollie/6.jpg",
+	"Ollie/7.jpg",
+	"Ollie/8.jpg",
+	"Ollie/9.jpg",
+	"Ollie/10.jpg"] // An array of photos
 
 var btn = document.querySelector("button"); //Searches in the HTML document for "button" and stores it in the variable btn
 var img = document.querySelector("img");
