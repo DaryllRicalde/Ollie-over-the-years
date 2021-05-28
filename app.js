065b5b43afb56f1f5f813e23ec39eeb6619e5f73
Daryll Ricalde <rstevendaryll@gmail.com>
2021-05-29 00:06:19 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var pics = [ 
+	"Ollie/1.jpg",
+	"Ollie/2.jpg",
+	"Ollie/3.jpg",
+	"Ollie/4.jpg",
+	"Ollie/5.jpg",
+	"Ollie/6.jpg",
+	"Ollie/7.jpg",
+	"Ollie/8.jpg",
+	"Ollie/9.jpg",
+	"Ollie/10.jpg",
+] // An array of photos
+
+var btn = document.querySelector("button"); //Searches in the HTML document for "button" and stores it in the variable btn
+var img = document.querySelector("img");
+var counter = 1;
+var size = pics.length;
+
+btn.addEventListener("click", function()
+{
+		img.src = pics[counter];
+		counter++;
+
+		if (counter === size){ // If the counter reaches the end of the pics array
+			counter = 0; // restart the counter
+		}
+})
